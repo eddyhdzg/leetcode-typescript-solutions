4b2911cef7b2bce24ee89be93a75751e6eee931d
Eddy Hernandez <eddyhdzg@gmail.com>
2022-01-26 21:11:44 -0600

    :sparkles: i'm back - graphs day 1

--- src/problems/547-Friend-Circles.ts
+++ src/problems/547-Friend-Circles.ts
@@ -1,65 +1,48 @@
-// JS
-// Runtime: 80 ms, faster than 91.63% of JavaScript online submissions for Friend Circles.
-// Memory Usage: 40.7 MB, less than 9.41% of JavaScript online submissions for Friend Circles.
-function findCircleNum(M: number[][]): number {
+function findCircleNum(isConnected: number[][]): number {
+  let circles = 0;
   const visited = new Set<number>();
 
-  let circles = 0;
-  for (let i = 0; i < M.length; i++) {
-    if (!visited.has(i)) {
-      dfs(i);
-
-      circles++;
-    }
-  }
-
-  return circles;
-
-  function dfs(i: number) {
-    for (let j = 0; j < M.length; j++) {
-      if (M[i][j] === 1 && !visited.has(j)) {
+  const dfs = (i: number) => {
+    for (let j = 0; j < isConnected.length; j++) {
+      if (isConnected[i][j] && !visited.has(j)) {
         visited.add(j);
         dfs(j);
       }
     }
+  };
+
+  for (let i = 0; i < isConnected.length; i++) {
+    if (!visited.has(i)) {
+      circles++;
+      dfs(i);
+    }
   }
+
+  return circles;
 }
 
-// Union find solution
-// function findCircleNum(M: number[][]): number {
-//   // Generate Graph
-//   const graph: { [key: string]: number[] } = {};
-//   for (let i = 0; i < M.length; i++) graph[i] = [];
-//   for (let i = 0; i < M.length; i++)
-//     for (let j = i + 1; j < M[0].length; j++)
-//       if (M[i][j] && i !== j) {
-//         graph[i].push(j);
-//         graph[j].push(i);
-//       }
+// JavaScript
+/*
+var findCircleNum = function (M) {
+  let circles = 0;
+  const visited = new Set();
 
-//   // Generate UF
-//   const uf: { [key: string]: number } = {};
-//   for (let i = 0; i < M.length; i++) uf[i] = i;
+  const dfs = (i) => {
+    for (let j = 0; j < M.length; j++) {
+      if (M[i][j] && !visited.has(j)) {
+        visited.add(j);
+        dfs(j);
+      }
+    }
+  };
 
-//   // Find
-//   const find = (x: number): number => {
-//     return uf[x] === x ? x : find(uf[x]);
-//   };
+  for (let i = 0; i < M.length; i++) {
+    if (!visited.has(i)) {
+      circles++;
+      dfs(i);
+    }
+  }
 
-//   // Union
-//   const union = (a: number, b: number) => {
-//     uf[find(a)] = uf[find(b)];
-//   };
-
-//   Object.entries(graph).forEach(([key, values]) => {
-//     values.forEach((value) => {
-//       union(parseInt(key), value);
-//     });
-//   });
-
-//   const set = new Set<number>();
-
-//   Object.keys(uf).forEach((key) => set.add(find(parseInt(key))));
-
-//   return set.size;
-// }
+  return circles;
+};
+*/
